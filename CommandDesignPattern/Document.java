d27a294043e2c5d200718ba23ef90a319d6647c5
new user <newuser@Alis-MacBook-Air.local>
2022-10-09 21:05:01 -0400

    write method

--- CommandDesignPattern/Document.java
+++ CommandDesignPattern/Document.java
@@ -1,18 +1,17 @@
 package CommandDesignPattern;
 
-
 import java.util.ArrayList;
 
 public class Document {
-   private String fileName;
+    private String fileName;
     private ArrayList<String> lines = new ArrayList<String>();
 
     public Document(String fileName) {
-        
+
     }
 
     public String view() {
-       return null;
+        return null;
     }
 
     public String append(String line) {
@@ -20,12 +19,17 @@ public class Document {
     }
 
     public String write(String line) {
-      return null;
+        lines.clear();
+        lines.add("________________");
+        lines.add(" ");
+        lines.add(" ");
+        lines.add("________________");
+        lines.add(lines.size() - 2, "| " + line + " |");
+        System.out.println("The line has been written to the file");
+        return "The line has been written to the file";
     }
 
     public String save() {
         return null;
     }
 }
-
-
